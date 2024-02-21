63248e13afc161b784b2b028bd8bdf38aae512cb
Ho Nyeong <dev.honing@gmail.com>
2024-02-21 11:19:16 +0900

    Feat: 회원가입/비밀번호 찾기 라우팅

--- Python/app.py
+++ Python/app.py
@@ -10,10 +10,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello')
-def hello():
-    return "Hello, Flask!"
+@app.route('/signup')
+def signup():
+    return "회원가입 페이지"
 
+
+@app.route('/forgot')
+def forgot():
+    return "비밀번호 찾기 페이지"
 
 if __name__ == '__main__':
     app.run(debug=True)
