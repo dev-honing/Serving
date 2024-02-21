c7cf660bbe815c72f9927fc6cdb26607dc2cf29f
Ho Nyeong <dev.honing@gmail.com>
2024-02-21 13:23:25 +0900

    Feat: MySQL 데이터베이스 연결 및 /users 페이지 라우팅
    - Jinja2 템플릿 엔진으로 HTML 템플릿 생성: users.html
    - DB 연결 및 라우팅 로직 추가

--- Python/app.py
+++ Python/app.py
@@ -1,11 +1,24 @@
 # 모듈 import
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
+import pymysql
 
 app = Flask(__name__)
 
+# MySQL 데이터베이스 연결
+db = pymysql.connect(host='localhost', user='root', password='1234', db='flask', charset='utf8')
 
+
+# 사용자 목록 확인 페이지
+@app.route('/users')
+def show_users_table():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM userInfo") # userInfo 테이블 렌더링
+    users = cursor.fetchall()
+    cursor.close()
+    return render_template("users.html", users=users)
+
+
+# 메인 페이지
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -17,20 +30,36 @@ def index():
         print(f"userID: {userID}")
         print(f"userPW: {userPW}")
 
+        # 데이터베이스 커서 생성
+        cursor = db.cursor()
+
+        # SQL 쿼리 실행
+        sql = "INSERT INTO userInfo (userID, userPW) VALUES (%s, %s)"
+        cursor.execute(sql, (userID, userPW))
+
+        # 데이터베이스에 적용
+        db.commit()
+
+        # 데이터베이스 커넥션 닫기
+        cursor.close()
+
         return "POST 처리 성공"
     else:
-        return render_template('index.html')
+        return render_template("index.html")
 
 
+# 회원가입 페이지
 @app.route('/signup')
 def signup():
     return "회원가입 페이지"
 
 
+# 비밀번호 찾기 페이지
 @app.route('/forgot')
 def forgot():
     return "비밀번호 찾기 페이지"
 
 
+# 서버 실행
 if __name__ == '__main__':
     app.run(debug=True)
