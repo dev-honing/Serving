6bdff2c289f260f28d95b50710addb05dbf0009d
Ho Nyeong <dev.honing@gmail.com>
2024-02-21 12:24:26 +0900

    Bad: userID, userPW를 출력에 성공했지만, 그 값이 None임

--- Python/app.py
+++ Python/app.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        # POST 요청에서 userID와 userPW를 가져오기
+        userID = request.form.get("ID")
+        userPW = request.form.get("PW")
+
+        # 서버 콘솔에 출력 테스트
+        print(f"userID: {userID}")
+        print(f"userPW: {userPW}")
+
         return "POST 처리 성공"
     else:
         return render_template('index.html')
