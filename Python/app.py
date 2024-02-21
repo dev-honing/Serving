237e6217a8d934393bda07eabc9191c29923c7e7
Ho Nyeong <dev.honing@gmail.com>
2024-02-21 11:26:59 +0900

    Feat: POST 요청 처리 성공
    - 같은 경로 /에 대한 요청 메서드를 다르게 설정해 예외 처리
    - @app.route() 메서드의 요청 메서드를 추가 작성해서 라우팅에 성공했다.

--- Python/app.py
+++ Python/app.py
@@ -1,13 +1,17 @@
 # 모듈 import
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        return "POST 처리 성공"
+    else:
+        return render_template('index.html')
 
 
 @app.route('/signup')
@@ -18,6 +22,7 @@ def signup():
 @app.route('/forgot')
 def forgot():
     return "비밀번호 찾기 페이지"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
